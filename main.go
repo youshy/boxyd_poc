73c92df1ed9635d7ef0a76219d06676d9287f971
youshy <devilyoushy@gmail.com>
2022-02-12 21:43:16 +0000

    add superbasic auth

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/sha256"
+	"crypto/subtle"
 	"log"
 	"net/http"
 
@@ -16,9 +18,32 @@ func main() {
 	// Create the route
 	router.
 		PathPrefix(staticDir).
-		Handler(http.StripPrefix(staticDir, http.FileServer(http.Dir("."+staticDir))))
+		Handler(basicAuth(http.StripPrefix(staticDir, http.FileServer(http.Dir("."+staticDir)))))
 
 	if err := http.ListenAndServe(":8080", router); err != nil {
 		log.Fatal("ListenAndServe Error: ", err)
 	}
+}
+
+func basicAuth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		username, password, ok := r.BasicAuth()
+		if ok {
+			usernameHash := sha256.Sum256([]byte(username))
+			passwordHash := sha256.Sum256([]byte(password))
+			expectedUsernameHash := sha256.Sum256([]byte("boxyd"))
+			expectedPasswordHash := sha256.Sum256([]byte("misiaptysia"))
+
+			usernameMatch := (subtle.ConstantTimeCompare(usernameHash[:], expectedUsernameHash[:]) == 1)
+			passwordMatch := (subtle.ConstantTimeCompare(passwordHash[:], expectedPasswordHash[:]) == 1)
+
+			if usernameMatch && passwordMatch {
+				next.ServeHTTP(w, r)
+				return
+			}
+		}
+
+		w.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+	})
 }
