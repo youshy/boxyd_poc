36002c7c68b4382fa87e23d0d9f21cf25a09e0f8
youshy <devilyoushy@gmail.com>
2022-02-13 08:36:58 +0000

    new shit, old shit

--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
 	"crypto/sha256"
 	"crypto/subtle"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
@@ -17,10 +20,17 @@ func main() {
 	// Choose the folder to serve
 	staticDir := "/boxes/"
 
+	// TODO: Figure out how to print the dir
+	/*
+		router.
+			Methods(http.MethodGet).
+			Handler(basicAuth(printIndex(staticDir)))
+	*/
+
 	// Create the route
 	router.
-		PathPrefix(staticDir).
-		Handler(basicAuth(http.StripPrefix(staticDir, http.FileServer(http.Dir("."+staticDir)))))
+		PathPrefix("/box/").
+		Handler(basicAuth(http.StripPrefix("/box/", http.FileServer(http.Dir("."+staticDir)))))
 
 	log.Println("Available routes:")
 	router.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
@@ -41,6 +51,48 @@ func main() {
 	if err := http.ListenAndServe(":8080", router); err != nil {
 		log.Fatal("ListenAndServe Error: ", err)
 	}
+}
+
+const titlepage = `
+<html>
+{{ range $i := .Body}}
+<a href={{$i}}>{{$i}}<br/></a>
+{{end}}
+</html>
+`
+
+type tp struct {
+	Title string
+	Body  []string
+}
+
+func printIndex(p string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var member []string
+		path := "." + p
+
+		err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				fmt.Println(err)
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+
+			if !info.IsDir() {
+				temp := strings.Replace(path, "boxes", "box", -1)
+				fmt.Printf("replaced from %s to %s\n", path, temp)
+				member = append(member, temp)
+			}
+			return nil
+		})
+		if err != nil {
+			fmt.Println(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		as := tp{Title: "Hello", Body: member}
+		t := template.Must(template.New("Index").Parse(titlepage))
+		t.Execute(w, as)
+	})
 }
 
 func basicAuth(next http.Handler) http.Handler {
