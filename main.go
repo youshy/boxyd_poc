1faf0fc5fa711068412199dc1a31e2ea482a1431
youshy <devilyoushy@gmail.com>
2022-02-13 07:27:18 +0000

    move u/p to env vars

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"crypto/sha256"
 	"crypto/subtle"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
@@ -20,6 +22,22 @@ func main() {
 		PathPrefix(staticDir).
 		Handler(basicAuth(http.StripPrefix(staticDir, http.FileServer(http.Dir("."+staticDir)))))
 
+	log.Println("Available routes:")
+	router.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
+		t, err := route.GetPathTemplate()
+		if err != nil {
+			return err
+		}
+		m, err := route.GetMethods()
+		if err != nil {
+			return err
+		}
+		fmt.Printf("%s\t%s\n", m, t)
+		return nil
+	})
+
+	log.Println("Starting server...")
+
 	if err := http.ListenAndServe(":8080", router); err != nil {
 		log.Fatal("ListenAndServe Error: ", err)
 	}
@@ -28,11 +46,13 @@ func main() {
 func basicAuth(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		username, password, ok := r.BasicAuth()
+		usernameCheck := os.Getenv("BOXYD_USERNAME")
+		passwordCheck := os.Getenv("BOXYD_PASSWORD")
 		if ok {
 			usernameHash := sha256.Sum256([]byte(username))
 			passwordHash := sha256.Sum256([]byte(password))
-			expectedUsernameHash := sha256.Sum256([]byte("boxyd"))
-			expectedPasswordHash := sha256.Sum256([]byte("misiaptysia"))
+			expectedUsernameHash := sha256.Sum256([]byte(usernameCheck))
+			expectedPasswordHash := sha256.Sum256([]byte(passwordCheck))
 
 			usernameMatch := (subtle.ConstantTimeCompare(usernameHash[:], expectedUsernameHash[:]) == 1)
 			passwordMatch := (subtle.ConstantTimeCompare(passwordHash[:], expectedPasswordHash[:]) == 1)
